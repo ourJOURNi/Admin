d352dae842ba51edb13a369791496cfd1ec79084
Eddie Taliaferro <eddielacrosse2@gmail.com>
2019-12-29 15:34:35 -0500

    routing of new pages

--- src/app/dashboard/dashboard-routing.module.ts
+++ src/app/dashboard/dashboard-routing.module.ts
@@ -1,12 +1,22 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { LogoutComponent } from './logout/logout.component';
 import { DashboardComponent } from './dashboard.component';
+import { JobsComponent } from './jobs/jobs.component';
+import { StudentsComponent } from './students/students.component';
+import { MentorsComponent } from './mentors/mentors.component';
+import { PostsComponent } from './posts/posts.component';
+import { EventsComponent } from './events/events.component';
+import { LogoutComponent } from './logout/logout.component';
 
 
 const routes: Routes = [
   { path: '', component: DashboardComponent },
-  { path: 'logout', component: LogoutComponent,  }
+  { path: 'jobs', component: JobsComponent },
+  { path: 'students', component: StudentsComponent },
+  { path: 'mentors', component: MentorsComponent },
+  { path: 'posts', component: PostsComponent },
+  { path: 'events', component: EventsComponent },
+  { path: 'logout', component: LogoutComponent }
 ];
 
 @NgModule({
