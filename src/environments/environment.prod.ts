0f9c934b5d6d3142659eb4cf786fe94203bb58a5
Eddie Taliaferro <eddielacrosse2@gmail.com>
2020-04-11 23:10:48 -0400

    AWS TEST

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,7 +3,7 @@
 import 'zone.js/dist/zone-error';
 
 const ip = '18.218.236.99';
-const baseUrl = `${ip}:3000`;
+const baseUrl = `http://${ip}:3000`;
 
 export const environment = {
   production: true,
